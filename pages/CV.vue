35072985beb26b5cc91cd0f39e1f12cec4db6d95
Sander van Ast <sandervanast1@gmail.com>
2021-01-13 20:29:10 +0100

    Starts by scrolling to the top

--- pages/CV.vue
+++ pages/CV.vue
@@ -1,45 +1,48 @@
 <template>
   <div class="Background">
     <h2>Download Resume</h2>
-    <button class="download">Download CV </button>
+    <button class="download">Download CV</button>
   </div>
 </template>
 
 <script>
 export default {
   layout: "nofooter",
-   methods:{
-       viewCV: function() {
-           window.location.href = "https://www.sandervanast.com/CV.pdf";
-       }
-   },
-   beforeMount(){
-      this.viewCV();
-   },
+  methods: {
+    viewCV: function () {
+      window.location.href = "https://www.sandervanast.com/CV.pdf";
+    },
+    ScrolltoTop() {
+      scrollTo({ scrollTop: 1200 }, "0");
+    },
+  },
+  beforeMount() {
+    this.scrollTop();
+    this.viewCV();
+  },
 };
 </script>
 
 <style lang="scss">
-
 .Background {
   border-radius: $borderRadius8;
   padding: 2.5rem 6rem;
   height: max-content;
-h2,
-h4 {
-  @include ColorTransition;
-  font-size: $fs-header * 1.3;
-  -webkit-text-stroke: transparent;
-  -webkit-text-fill-color: var(--TxtColor);
-  opacity: $WmarkOpacity * 5.8;
-  &::after {
-    content: ";";
-    -webkit-text-fill-color: $OrangeColor1;
+  h2,
+  h4 {
     @include ColorTransition;
+    font-size: $fs-header * 1.3;
+    -webkit-text-stroke: transparent;
+    -webkit-text-fill-color: var(--TxtColor);
+    opacity: $WmarkOpacity * 5.8;
+    &::after {
+      content: ";";
+      -webkit-text-fill-color: $OrangeColor1;
+      @include ColorTransition;
+    }
   }
-}
-.download {
+  .download {
     @include ButtonStyle;
-}
+  }
 }
 </style>
