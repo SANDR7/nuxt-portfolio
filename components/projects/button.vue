69119e550ec875b1db689aa335265bfffccb813e
Sander van Ast <sandervanast1@gmail.com>
2021-02-16 16:08:10 +0100

    button for projects

---
+++ components/projects/button.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="margin: 1em 0; margin-top: 6em" class="OverView">
+    <nuxt-link to="/Projects" style="font-size: 1.5em; padding-bottom: 0.3em"
+      ><i class="far fa-th"></i> Overview of Projects</nuxt-link
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectBtns",
+};
+</script>
+
+<style>
+</style>
