e5cd8c0195d5c5517efce97e97a4e68aa1c7860a
Sander van Ast <sandervanast1@gmail.com>
2021-02-17 13:27:11 +0100

    :sparkles: implementing dark mode toggle

--- layouts/partials/nav.vue
+++ layouts/partials/nav.vue
@@ -1,9 +1,25 @@
 <template>
   <header>
-      <div id="nav">
-          <!-- <div class="ToggleTheme" @click="toggle" style="cursor: pointer" title="Change theme">
-            <i class="fas fa-adjust"></i>
-          </div> -->
+    <!-- <header style="margin-bottom: 8rem"> -->
+    <div id="nav">
+      <div
+        class="ToggleTheme"
+        @click="toggle"
+        style="cursor: pointer"
+        :title="`Current theme is ${this.darkMode}`"
+      >
+        <i
+          class="fas fa-adjust"
+          :style="
+            this.isToggled
+              ? 'transform: rotate(180deg)'
+              : 'transform: rotate(0deg)'
+          "
+        ></i>
+        <div class="ToggleText">
+          Change theme to {{ this.isToggled ? "light" : "dark" }}
+        </div>
+      </div>
       <div class="NavLinks" @click="ScrolltoTop">
         <nuxt-link to="/" title="Home Page">Home</nuxt-link>
         <nuxt-link to="/Projects" title="Projects Page">Projects</nuxt-link>
@@ -15,93 +31,105 @@
 </template>
 
 <script>
-
-
 export default {
   data() {
     return {
-      darkMode: 'light',
-    }
+      darkMode: "", // default value (system preference)
+      isToggled: true,
+    };
   },
-   methods: {
+  methods: {
     toggle() {
-      if(this.darkMode == 'light') {
-        this.darkMode = 'dark'
-
+      if (this.darkMode == "light") {
+        this.darkMode = "dark";
+        this.isToggled = true;
       } else {
-        this.darkMode = 'light'
+        this.darkMode = "light";
+        this.isToggled = false;
       }
     },
     ScrolltoTop() {
-      scrollTo({scrollTop:1200},'0');
+      scrollTo({ scrollTop: 1200 }, "0");
     },
-    },
-    head () {
-      return {
-        bodyAttrs: {
-          class: this.darkMode
-        }
-      }
-    },
-}
+  },
+  head() {
+    return {
+      bodyAttrs: {
+        class: this.darkMode,
+      },
+    };
+  },
+};
 </script>
 
 <style lang="scss">
 #nav {
+  display: flex;
+  // position: fixed;
+  // top: 0;
+  justify-content: space-between;
+  margin: 1rem 1rem 1.5rem;
+  padding: 1rem 3rem;
+  z-index: 7 !important;
+  // background-color: var(--BackgroundNavColor);
+  .ToggleTheme {
+    @include subCardBackground;
     display: flex;
-    justify-content: space-between;
-    margin: 1rem 1rem 1.5rem;
-    padding: 1rem 3rem;
-    .ToggleTheme {
-      .fa-adjust {
-          font-size: $fs-header / 1.5;
+    align-items: center;
+    padding: 0.7rem 1rem !important;
+    .ToggleText {
+      width: max-content;
+    }
+    .fa-adjust {
+      font-size: $fs-header / 1.5;
+      margin-right: 0.5em;
+      @include ColorTransition;
+    }
+  }
+  .NavLinks {
+    display: block;
+    a {
+      color: var(--TxtColor);
+      font-size: 18px;
+      margin: 0 1.5rem;
+      text-decoration: none !important;
+      outline: none;
+
+      padding: 0.5rem 1rem;
+      @include HoverFx(3px, $OrangeColor1, 0);
+    }
+    .nuxt-link-exact-active {
+      &::after {
+        content: "";
+        position: absolute;
+        left: 0;
+        bottom: -1px;
+        width: 100%;
+        height: 3px;
+        background-color: $OrangeColor1;
+
+        // @include ColorTransition;
+        transform-origin: right;
+        transform: scaleX(1);
+        transition: transform 0.3s cubic-bezier(0, 0.79, 0.79, 0.93);
       }
     }
-    .NavLinks {
-        display: block;
-        a {
-          color: var(--TxtColor);
-          font-size: 18px;
-          margin: 0 2rem;
-          text-decoration: none !important;
-          outline: none;
-          
-          padding: .5rem 1rem;
-          @include HoverFx(3px, $OrangeColor1, 0);
-        }
-        .nuxt-link-exact-active {
-            &::after {
-              content: '';
-              position: absolute;
-              left: 0;
-              bottom: -1px;
-              width: 100%;
-              height: 3px;
-              background-color: $OrangeColor1;
-      
-              // @include ColorTransition;
-              transform-origin: right;
-              transform: scaleX(1);
-              transition: transform .3s cubic-bezier(0,.79,.79,.93);
-            }
-        }
-    }
+  }
 }
 /* Mobile ----------- */
 @media only screen and (min-width: $MobileSize1) {
-  #nav{
+  #nav {
     display: inline-block;
     width: 80%;
     overflow-x: auto;
-    scrollbar-width: thin; /* Firefox */
     align-items: center;
     .ToggleTheme {
       position: absolute;
-      padding:0rem 0;
+      padding: 0rem 0;
     }
     .NavLinks {
       height: 10vh;
-      width: 100vw;
+      width: 100%;
       overflow-x: auto;
       scrollbar-width: thin; /* Firefox */
       display: flex;
@@ -110,13 +138,12 @@ export default {
       left: 0%;
       background-color: var(--BackgroundNavColor);
       align-items: center;
-      z-index: 5;
     }
   }
 }
 /* Tablet ----------- */
 @media only screen and (min-width: $TabletSize1) {
-  #nav{
+  #nav {
     display: flex;
     width: 100%;
     .ToggleTheme {
@@ -135,14 +162,14 @@ export default {
 @media only screen and (min-width: $Laptop1) {
   #nav {
     margin: 0rem 1rem 1.5rem;
-    padding: .8rem 1rem;
+    padding: 0.8rem 1rem;
   }
 }
 /* Desktop ----------- */
 @media only screen and (min-width: $LargeScreen1) {
   #nav {
-    margin: 0rem 1rem 1.5rem ;
-    padding: .8rem 3rem;
+    margin: 0rem 1rem 1.5rem;
+    padding: 0.8rem 3rem;
   }
 }
 </style>
