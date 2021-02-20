bbab2c1108b461796cde84722adf17dfac6cafe0
Sander van Ast <sandervanast1@gmail.com>
2021-02-20 14:56:26 +0100

    the start of a new section

---
+++ components/about/GithubSection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img
+      align="center"
+      src="https://github-readme-stats.vercel.app/api/top-langs/?username=SANDR7&layout=compact&hide_title=1&card_width=300&theme=dark"
+    />
+    <img
+      align="center"
+      src="https://github-readme-stats.vercel.app/api?username=SANDR7&show_icons=true&line_height=27&count_private=true&title_color=ffffff&text_color=ffeeff&icon_color=FF7B47&bg_color=1d1f21"
+    />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
