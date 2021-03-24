88fc4d3618e90820924a74c77d1c08d730952a8d
Sander van Ast <sandervanast1@gmail.com>
2021-03-24 22:29:04 +0100

    :alembic: search feature

--- components/about/GithubSection.vue
+++ components/about/GithubSection.vue
@@ -1,38 +1,50 @@
 <template>
-  <div class="Github">
-    <div class="Card">
-      <img :src="gitData.avatar_url" :alt="gitData.login" />
-      <div class="info">
-        <div style="padding-bottom: 2em">
-          <span>{{ gitData.name }}</span>
-          <h3 class="loginName">{{ gitData.login }}</h3>
-          <div>Currently {{ gitData.followers }} followers</div>
-          <a :href="gitData.html_url" target="blank">Full page</a>
-        </div>
-        <span class="title">latest repos</span
-        ><span>&nbsp;{{ this.shownRepos }}/{{ gitData.public_repos }}</span>
-        <div
-          v-for="(data, index) in gitReData.slice(0, this.shownRepos)"
-          :key="index"
-        >
-          <li class="reposes">{{ gitReData[index].name }}</li>
-        </div>
-      </div>
-      <div></div>
-    </div>
-    <div class="teamMates">
-      <div class="header">people I collaborate with</div>
-      <div class="Arr">
-        <div v-for="(data, index) in gitFoData" :key="index" class="ArrCard">
-          <div>
-            <img
-              height="150rem"
-              :src="gitFoData[index].avatar_url"
-              :alt="gitFoData.login"
-            />
+  <div>
+    <input type="text" v-model="InputUser"/>
+    <button @click="test">submit</button>
+    {{InputUser}}
+    <div class="Github">
+      <div class="Card">
+        <img :src="gitData.avatar_url" :alt="gitData.login" />
+        <div class="info">
+          <div style="padding-bottom: 2em">
+            <span>{{ gitData.name }}</span>
+            <h3 class="loginName">{{ gitData.login }}</h3>
+            <div>Currently {{ gitData.followers }} followers</div>
+            <a :href="gitData.html_url" target="blank">Full Github page</a>
           </div>
-          <div style="margin: .5em 0">
-            {{ gitFoData[index].login }}
+          <span class="title">latest repos</span
+          ><span>&nbsp;{{ this.shownRepos }}/{{ gitData.public_repos }}</span>
+            <!-- v-for="(data, index) in gitReData.slice(0, this.shownRepos)" -->
+          <div
+            v-for="(data, index) in gitReData"
+            :key="index"
+          >
+            <li class="reposes">{{ gitReData[index].name }}</li>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div class="teamMates">
+        <div class="header">people I collaborate with</div>
+        <div class="Arr">
+          <div v-for="(data, index) in gitFoData" :key="index" class="ArrCard">
+            <div>
+              <img
+                height="150rem"
+                :src="gitFoData[index].avatar_url"
+                :alt="gitFoData.login"
+              />
+            </div>
+            <div style="margin: 0.5em 0">
+              <a
+                :href="gitFoData.html_url"
+                target="blank"
+                style="padding-bottom: 0.4em"
+              >
+                {{ gitFoData[index].login }}
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -48,20 +60,23 @@ export default {
       gitData: [],
       gitReData: [],
       gitFoData: [],
-      user: "sandr7"
+      user: "Sandr7",
     };
+  },
+  test() {
+    this.user = this.InputUser;
   },
   async fetch() {
     this.gitData = await fetch(
       `https://api.github.com/users/${this.user}`
-    ).then(res => res.json());
+    ).then((res) => res.json());
     this.gitReData = await fetch(
       `https://api.github.com/users/${this.user}/repos`
-    ).then(res => res.json());
+    ).then((res) => res.json());
     this.gitFoData = await fetch(
       `https://api.github.com/users/${this.user}/following`
-    ).then(res => res.json());
-  }
+    ).then((res) => res.json());
+  },
 };
 </script>
 
@@ -109,10 +124,11 @@ export default {
       width: 100%;
       display: grid;
       grid-template-columns: repeat(3, 33%);
-      grid-gap: 2em;
+      grid-row-gap: 2em;
+      grid-column-gap: 0.5em;
       .ArrCard {
         @include CardBackground;
-        padding: 0.3em;
+        padding: 0.6em;
         margin-right: 2em;
         text-align: center;
         img {
